b3a20db3c4b84493e176680eb86f7b971441da4e
Maxim Zasorin <maximzasorin@gmail.com>
2017-08-26 23:58:46 +0300

    minor improvements

--- expose/node.sh
+++ expose/node.sh
@@ -1,17 +1,22 @@
 #/bin/bash
 
-unzip -o /tmp/data/data.zip -d /data/initial
+sleep 10
+
+unzip -o /tmp/data/data.zip -d /data/initial &> /dev/null
 
 for filename in /data/initial/users_*.json; do
-	cat $filename | jq '.users' | mongoimport --db travels --collection users --jsonArray
+	cat $filename | jq '.users' | mongoimport --db travels --collection users --jsonArray &> /dev/null
+	echo "import $filename"
 done
 
 for filename in /data/initial/locations_*.json; do
-	cat $filename | jq '.locations' | mongoimport --db travels --collection locations --jsonArray
+	cat $filename | jq '.locations' | mongoimport --db travels --collection locations --jsonArray &> /dev/null
+	echo "import $filename"
 done
 
 for filename in /data/initial/visits_*.json; do
-	cat $filename | jq '.visits' | mongoimport --db travels --collection visits --jsonArray
+	cat $filename | jq '.visits' | mongoimport --db travels --collection visits --jsonArray &> /dev/null
+	echo "import $filename"
 done
 
 npm run prod
